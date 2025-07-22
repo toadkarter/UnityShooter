661c7a772bdd53a15f2d09c34f1a4e83c5969c98
toadkarter <vrakhmanin1@gmail.com>
2025-07-22 18:11:11 +0100

    Fixes issue with triggers being on the interact layer.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [field: SerializeField] public ParticleSystem MuzzleFlash { get; private set; } 
+    [field: SerializeField] public LayerMask interactionLayers { get; private set; } 
     
     public void Shoot(WeaponSO weaponDetails)
     {
         MuzzleFlash.Play();
         
         RaycastHit hit;
-        bool hitFound = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity);
+        bool hitFound = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, interactionLayers, QueryTriggerInteraction.Ignore);
 
         if (hitFound)
         {
